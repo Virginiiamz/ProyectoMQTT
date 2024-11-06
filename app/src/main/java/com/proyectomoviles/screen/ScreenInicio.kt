2a10d4955bcd3e420087cab1365ad1b986b12535
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2024-11-06 16:24:00 +0100

    Creacion de la pantalla Inicio

---
+++ app/src/main/java/com/proyectomoviles/screen/ScreenInicio.kt
@@ -0,0 +1,9 @@
+package com.proyectomoviles.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InicioScreen() {
+    Text("Holaaaaaa")
+}
