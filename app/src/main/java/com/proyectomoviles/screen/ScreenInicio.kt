8c60f00ede5c0622bbe94725f04cab3c9c6593ea
Virginiiamz <virginia.munoz-castro@iesruizgijon.com>
2024-11-06 17:28:05 +0100

    Creacion de boton añadir en en Inicio

--- app/src/main/java/com/proyectomoviles/screen/ScreenInicio.kt
+++ app/src/main/java/com/proyectomoviles/screen/ScreenInicio.kt
@@ -1,11 +1,47 @@
 package com.proyectomoviles.screen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.proyectomoviles.dispositivos.Temperatura
 
 @Composable
 fun InicioScreen() {
-    val temperatura = Temperatura(25.03, 75.05)
-    Text(temperatura.toString())
+
+    Scaffold(
+        floatingActionButton = {
+            MyFloatingActionButton()
+        },
+        floatingActionButtonPosition = FabPosition.Start
+    ) {
+        paddingValue ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(paddingValue)
+        ) {
+
+        }
+    }
+
+    //val temperatura = Temperatura(25.03, 75.05)
+    //Text(temperatura.toString())
+}
+
+@Composable
+fun MyFloatingActionButton() {
+    FloatingActionButton(
+        onClick = {},
+        containerColor = MaterialTheme.colorScheme.secondary
+    ) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+    }
 }
