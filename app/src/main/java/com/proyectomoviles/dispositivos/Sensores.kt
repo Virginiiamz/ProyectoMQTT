58d1b9accf48b0b2f0b044a6c8e646de17abfbef
Javier Castillo <javiercastillolimon@gmail.com>
2024-11-06 16:58:55 +0100

    .

---
+++ app/src/main/java/com/proyectomoviles/dispositivos/Sensores.kt
@@ -0,0 +1,9 @@
+package com.proyectomoviles.dispositivos
+
+data class Temperatura(
+    val temperatura: Double,
+    val humedad: Double,
+)
+
+
+
--- app/src/main/java/com/proyectomoviles/screen/ScreenInicio.kt
+++ app/src/main/java/com/proyectomoviles/screen/ScreenInicio.kt
@@ -2,8 +2,10 @@ package com.proyectomoviles.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.proyectomoviles.dispositivos.Temperatura
 
 @Composable
 fun InicioScreen() {
-    Text("Holaaaaaa")
+    val temperatura = Temperatura(25.03, 75.05)
+    Text(temperatura.toString())
 }
