0f56242fae056f24e92d9d085bc53c83bddc5243
AdrianJMACHADO <xajuagar672@ieshnosmachado.org>
2024-11-18 19:27:42 +0100

    Subo mi cambio

--- app/src/main/java/com/proyectomoviles/MainActivity.kt
+++ app/src/main/java/com/proyectomoviles/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -25,6 +26,12 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun llamarNavegacio() {
+    Navegacion()
 }
 
 @Composable
--- app/src/main/java/com/proyectomoviles/screen/ScreenElementos.kt
+++ app/src/main/java/com/proyectomoviles/screen/ScreenElementos.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -17,15 +18,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.proyectomoviles.dispositivos.Dispositivo
 import kotlinx.serialization.Serializable
+import com.proyectomoviles.R
 
 @Serializable
 object Elementos
 
 @Composable
 fun ElementosScreen() {
+    val dispositivos = listOf(
+        "Sensor de temperatura",
+        "Sensor de luz",
+        "Sensor de movimiento",
+        "Sensor de vibracion"
+    )
+
+    val dispositivoImagenMap = mapOf(
+        "Sensor de temperatura" to R.drawable.imgtermometro,
+        "Sensor de luz" to R.drawable.imgsensorluz,
+        "Sensor de movimiento" to R.drawable.imgsensormovimiento,
+        "Sensor de vibracion" to R.drawable.imgsensorvibracion
+    )
 
     Scaffold(
         floatingActionButton = { },
@@ -39,34 +55,36 @@ fun ElementosScreen() {
             verticalArrangement = Arrangement.spacedBy(8.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            if (Dispositivo.dispositivosCreados.isEmpty()) {
+            if (dispositivos.isEmpty()) {
                 item {
                     Text("No hay dispositivos vinculados")
                 }
-            } else {
-                items(Dispositivo.dispositivosCreados) { dispositivo ->
-                    DispositivoButton(dispositivo.nombre)
+            } /*else {
+                items(dispositivos) { dispositivo ->
+                    val imageRes = dispositivoImagenMap[dispositivo] ?: R.drawable.error
+                    DispositivoButton(nombre = dispositivo, imageRes = imageRes)
                 }
-            }
+            }*/
         }
     }
 }
 
+
 @Composable
-fun DispositivoButton(nombre: String) {
+fun DispositivoButton(nombre: String, imageRes: Int) {
     Button(
-        onClick = {  },
+        onClick = { },
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
         contentPadding = PaddingValues(vertical = 8.dp)
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-/*            Image(
-                imageVector = ImageVector.vectorResource(id = R.drawable.ic_sample_icon), // Cambia 'ic_sample_icon' por el ID de tu icono
-                contentDescription = null,
-                modifier = Modifier.size(24.dp)
-            )*/
+            androidx.compose.foundation.Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = "Icono de $nombre",
+                modifier = Modifier.size(48.dp)
+            )
             Spacer(modifier = Modifier.height(4.dp))
             Text(text = nombre)
         }
