650caf1bda0952a7843d7e536b9411b7b851b13b
Javier Castillo <javiercastillolimon@gmail.com>
2024-11-21 18:03:25 +0100

    Creada clase actuadores

--- app/src/main/java/com/proyectomoviles/dispositivos/Sensores.kt
+++ app/src/main/java/com/proyectomoviles/dispositivos/Sensores.kt
@@ -56,17 +56,6 @@ class SensorPresion(
     }
 }
 
-class ActuadorValvula(
-    nombre: String,
-    tipo: String,
-    ubicacion: String,
-    val activo: Boolean
-): Dispositivo(nombre, tipo, ubicacion) {
-    init {
-        dispositivosCreados.add(this)
-    }
-}
-
 class SensorApertura(
     nombre: String,
     tipo: String,
